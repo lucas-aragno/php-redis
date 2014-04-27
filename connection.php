5b09b61f27ef92cd15c61c91496950a033c151ac
laragno <laragno@globants-MacBook.local>
2014-04-27 11:21:25 -0300

    added basic conection script styles and html structure

---
+++ connection.php
@@ -0,0 +1,16 @@
+<?php
+
+define('HOST','127.0.0.1');
+define('PORT',6379);
+define ('DB',0);
+
+try{
+	$redis = new Redis();
+	$redis -> connect(HOST,PUERTO);
+	$redis -> select(DB);
+} catch (Exception $e){
+	die('ERROR'.$e -> getCode().':'.$e -> getMessage());
+
+}
+
+?>
